13698dc2450d935a447a762d20b2500924ac049f
LBoyle <louis_boyle@hotmail.com>
2017-06-10 14:54:46 +0100

    fixed images

--- app/serializers/part_serializer.rb
+++ app/serializers/part_serializer.rb
@@ -1,6 +1,6 @@
 class PartSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :price, :spec
+  attributes :id, :name, :description, :price, :spec, :image
   has_many :parttypes
   has_many :incompatibilities
-  
+
 end
